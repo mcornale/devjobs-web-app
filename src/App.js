fb0405225e13ea509bf00e9cb6381de08da9d0d0
mcornale <massimiliano.cornale@gmail.com>
2021-12-10 21:34:09 +0100

    Add smooth scroll behaviour

--- src/App.js
+++ src/App.js
@@ -7,48 +7,51 @@ import HomePage from './pages/HomePage';
 import JobPage from './pages/JobPage';
 
 import styles from './App.module.css';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
   return (
     <div className={styles.app} data-theme='light'>
       <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <BackgroundSVG />
-                <header>
-                  <Logo />
-                  <Toggle />
-                </header>
-                <HomePage />
-              </>
-            }
-          />
-          <Route
-            path=':jobId'
-            element={
-              <>
-                <BackgroundSVG />
-                <header>
-                  <Logo />
-                  <Toggle />
-                </header>
-                <JobPage />
-              </>
-            }
-          />
+        <ScrollToTop>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <BackgroundSVG />
+                  <header>
+                    <Logo />
+                    <Toggle />
+                  </header>
+                  <HomePage />
+                </>
+              }
+            />
+            <Route
+              path=':jobId'
+              element={
+                <>
+                  <BackgroundSVG />
+                  <header>
+                    <Logo />
+                    <Toggle />
+                  </header>
+                  <JobPage />
+                </>
+              }
+            />
 
-          <Route
-            path='*'
-            element={
-              <main style={{ paddingTop: '4rem' }}>
-                <h3>Sorry! Page not found</h3>
-              </main>
-            }
-          />
-        </Routes>
+            <Route
+              path='*'
+              element={
+                <main style={{ paddingTop: '4rem' }}>
+                  <h3>Sorry! Page not found</h3>
+                </main>
+              }
+            />
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
